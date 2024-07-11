f6cc497d1f650843f6d72e971bfe8ead36c86f9f
Igor Dm <gid68@mail.ru>
2024-07-11 09:11:19 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,14 @@
+import true_math
+
+
+def divide(first, second):
+    """
+
+    :param first:
+    :param second:
+    :return first / second
+    """
+    if second == 0:
+        return 'Ошибка'
+    return first / second
+# print(__name__)
